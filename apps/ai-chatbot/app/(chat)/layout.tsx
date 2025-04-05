c0297d82a4a621f6deb0ba0a1d9e0b7c358b0b4a
xn1cklas <nicklas.scharpff@nsquare.one>
2025-04-05 10:41:29 +0200

    feat: vercel ai chatbot minimal setup

---
+++ apps/ai-chatbot/app/(chat)/layout.tsx
@@ -0,0 +1,22 @@
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Script from "next/script";
+
+export const experimental_ppr = true;
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Script
+        src="https://cdn.jsdelivr.net/pyodide/v0.23.4/full/pyodide.js"
+        strategy="beforeInteractive"
+      />
+      <TooltipProvider delayDuration={0}>
+        <div>{children}</div>
+      </TooltipProvider>
+    </>
+  );
+}
